82400f5b78c2c75c0062b56fedf5564903e1716c
Nic Pincombe <npincombe@Sparky.local>
2011-05-25 14:54:03 +1000

    implemented collection interface

--- Circle.java
+++ Circle.java
@@ -2,6 +2,6 @@ public class Circle implements Geometry{
   private int x, y;
   private int radius;
   public void addToDevice(Device device){
-    device.getJComponent().getGraphics().drawOval(x, y, radius, radius);
+    device.getJComponent().getGraphics().drawOval(x, y, radius*2, radius*2);
   }
 }
--- GeometryCollection.java
+++ GeometryCollection.java
@@ -1,17 +1,62 @@
 import java.util.Collection;
 import java.util.ArrayList;
+import java.util.Iterator;
 
-public abstract class GeometryCollection<E extends Geometry>
-// implements Collection<E>
-{
-  private ArrayList<E> list = new ArrayList<E>();
-  
-  public abstract void draw(); //change to accept a device if necessary
+public abstract class GeometryCollection<E extends Geometry> implements Collection<E> {
+    private ArrayList<E> list = new ArrayList<E>();
 
-  public boolean add(E e){
-    return list.add(e);
-  }  
+    public abstract void draw(); //change to accept a device if necessary
 
-  //implement remaining methods from collection
+    public int size() {
+        return list.size();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    public boolean contains(Object o) {
+        return list.contains(o);
+    }
+
+    public Iterator<E> iterator() {
+        return list.iterator();
+    }
+
+    public Object[] toArray() {
+        return list.toArray();
+    }
+
+    public <T> T[] toArray(T[] ts) {
+        return list.toArray(ts);
+    }
+
+    public boolean add(E e) {
+        return list.add(e);
+    }
+
+    public boolean remove(Object o) {
+        return list.remove(o);
+    }
+
+    public boolean containsAll(Collection<?> objects) {
+        return list.containsAll(objects);
+    }
+
+    public boolean addAll(Collection<? extends E> es) {
+        return list.addAll(es);
+    }
+
+    public boolean removeAll(Collection<?> objects) {
+        return list.removeAll(objects);
+    }
+
+    public boolean retainAll(Collection<?> objects) {
+        return list.retainAll(objects);
+    }
+
+    public void clear() {
+        list.clear();
+    }
 }
 
